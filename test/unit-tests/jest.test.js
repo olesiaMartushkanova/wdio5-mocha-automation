7b8f233795c08aca048ad7bf664bafa4de658d66
olesiaMartushkanova <olesia.martushkanova@gmail.com>
2020-10-30 13:59:04 +1300

    testing github action on PR

--- test/unit-tests/jest.test.js
+++ test/unit-tests/jest.test.js
@@ -2,8 +2,6 @@ const sum = require('./sum');
 
 describe('sum function', () => {
   test('adds 1 + 2 to equal 3', () => {
-    // I want my action to fail
-    expect(sum(1, 2)).toBe(4);
-    //  expect(sum(1, 2)).toBe(3);
+    expect(sum(1, 2)).toBe(3);
   });
 });
